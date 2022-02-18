cb07a8e8913ac376ad0325b20c9e520b44300583
slava <slavaborodin07@gmail.com>
2022-02-18 13:42:19 +0100

    front end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup} from "@angular/forms";
+
+class FormConrol extends AbstractControl {
+  patchValue(value: any, options?: Object): void {
+  }
+
+  reset(value?: any, options?: Object): void {
+  }
+
+  setValue(value: any, options?: Object): void {
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +18,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'untitled';
+  title = 'priklad2';
+  cenaKs: number = 0;
+  mnozstvo: number = 0;
+  spolu: number = 0;
+
+  form: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup({
+      cenaKs: new FormControl(),
+      mnozstvo: new FormControl()
+    } )
+  }
+
+  public pracuj(): void{
+    alert("Ahoj!");
+  }
+  public pisem1(e:any): void{
+    this.cenaKs = e.target.value;
+  }
+  public pisem2(e:any): void {
+    this.mnozstvo = e.target.value;
+  }
 }
