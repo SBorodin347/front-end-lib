cc2db30100211a5d36a362df3d0abff1ae516ae2
domca <domca.kamiencok@gmail.com>
2022-02-19 17:12:51 +0100

    [FINAL]pridana funkcionalita "zaskrtavania" checkboxov, pridany reset button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import {AbstractControl, FormControl, FormGroup} from "@angular/forms";
+import { Component, QueryList, ViewChildren, ElementRef} from '@angular/core';
+import {AbstractControl, FormGroup} from "@angular/forms";
 
-class FormConrol extends AbstractControl {
+class FormControl extends AbstractControl {
   patchValue(value: any, options?: Object): void {
   }
 
@@ -44,24 +44,59 @@ export class AppComponent {
 
   //ULOHA 1
   dec: number = 0;
-  public prevod(e:any): void{
-    this.dec = e.target.value;
-  }
 
   public dec2bin(){
     return (this.dec >>> 0).toString(2);
   }
 
   bin: number = 0;
+  id: any;
+
   checkCheckBoxvalue(event: any){
+    this.id = event.target.id;
     if(event.target.checked) {
       this.bin = parseInt(event.target.value);
       this.dec += this.bin;
-      console.log(this.bin);
+    }else{
+      this.bin = parseInt(event.target.value);
+      this.dec -= this.bin;
     }
-    console.log(event.target.checked)
+    // console.log(event.target.id);
+    // this.id = document.getElementById('B1');
+    // console.log(this.id.value); //value podla ID
   }
 
+  public prevod(): void{
+    let array = [];
+    let binary = this.dec2bin();
+    binary = binary.split("").reverse().join(""); // You need to reverse the string to get the power of 2 corresponding
+    for(let i = binary.length - 1; i >= 0; i--){
+      if(binary[i] == "1"){
+        array.push(Math.pow(2, i));
+      }
+    }
+    console.log(array); // Check the array
 
+    for(let i = array.length - 1; i >= 0; i--){
+      let element = <HTMLInputElement> document.getElementById(array[i].toString());
+      let val = parseInt(element.value);
+      console.log(i, val);
+      // console.log(i, element);
+      if(array[i] == val){
+        element.checked = true;
+        console.log(array[i], val);
+      }
+    }
+  }
+
+  @ViewChildren("checkboxes")
+  checkboxes!: QueryList<ElementRef>;
+
+  public reset() {
+    this.checkboxes.forEach((element) => {
+      element.nativeElement.checked = false;
+    });
+    this.dec = 0;
+  }
 
 }
