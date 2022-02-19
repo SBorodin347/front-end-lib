8d169771b047949ea242e3b62b6ea0d24ed84933
domca <domca.kamiencok@gmail.com>
2022-02-19 17:18:47 +0100

    [FINAL]pridana funkcionalita "zaskrtavania" checkboxov, pridany reset button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,24 @@
 import { Component, QueryList, ViewChildren, ElementRef} from '@angular/core';
-import {AbstractControl, FormGroup} from "@angular/forms";
-
-class FormControl extends AbstractControl {
-  patchValue(value: any, options?: Object): void {
-  }
-
-  reset(value?: any, options?: Object): void {
-  }
-
-  setValue(value: any, options?: Object): void {
-  }
-}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'priklad2';
-  cenaKs: number = 0;
-  mnozstvo: number = 0;
-  spolu: number = 0;
-
-  form: FormGroup;
-
-  constructor() {
-    this.form = new FormGroup({
-      cenaKs: new FormControl(),
-      mnozstvo: new FormControl()
-    } )
-  }
-
-  public pracuj(): void{
-    alert("Ahoj!");
-  }
-  public pisem1(e:any): void{
-    this.cenaKs = e.target.value;
-  }
-  public pisem2(e:any): void {
-    this.mnozstvo = e.target.value;
-  }
-
-  //ULOHA 1
+  title = 'DECIMAL TO BINARY (a naopak)';
   dec: number = 0;
+  bin: number = 0;
+  id: any;
 
+  // PREVOD DO BIN
   public dec2bin(){
     return (this.dec >>> 0).toString(2);
   }
 
-  bin: number = 0;
-  id: any;
-
-  checkCheckBoxvalue(event: any){
+  // ZMENA NA ZAKLADE CHECKBOXOV (BIN >> DEC)
+  public checkCheckBoxvalue(event: any){
     this.id = event.target.id;
     if(event.target.checked) {
       this.bin = parseInt(event.target.value);
@@ -61,30 +27,25 @@ export class AppComponent {
       this.bin = parseInt(event.target.value);
       this.dec -= this.bin;
     }
-    // console.log(event.target.id);
-    // this.id = document.getElementById('B1');
-    // console.log(this.id.value); //value podla ID
   }
 
+  // ZMENA NA ZAKLADE INPUTU (DEC >> BIN)
   public prevod(): void{
     let array = [];
     let binary = this.dec2bin();
-    binary = binary.split("").reverse().join(""); // You need to reverse the string to get the power of 2 corresponding
+    binary = binary.split("").reverse().join(""); // reverse
     for(let i = binary.length - 1; i >= 0; i--){
       if(binary[i] == "1"){
         array.push(Math.pow(2, i));
       }
     }
-    console.log(array); // Check the array
 
     for(let i = array.length - 1; i >= 0; i--){
       let element = <HTMLInputElement> document.getElementById(array[i].toString());
       let val = parseInt(element.value);
-      console.log(i, val);
-      // console.log(i, element);
       if(array[i] == val){
         element.checked = true;
-        console.log(array[i], val);
+        // console.log(array[i], val);
       }
     }
   }
@@ -92,6 +53,7 @@ export class AppComponent {
   @ViewChildren("checkboxes")
   checkboxes!: QueryList<ElementRef>;
 
+  // RESET CHECKBOXOV A DEC
   public reset() {
     this.checkboxes.forEach((element) => {
       element.nativeElement.checked = false;
