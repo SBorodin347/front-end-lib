178ce9a28cdb3b570c3470d8293d20d5923d33fe
domca <domca.kamiencok@gmail.com>
2022-03-03 22:10:56 +0100

    customer branch, vytvorene components aj routovanie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component} from '@angular/core';
+import {Router} from "@angular/router"
 
 enum MENU{
   USERS,BOOKS,BORROWINGS
@@ -13,34 +14,14 @@ enum MENU{
 export class AppComponent {
   title = 'LIBRARY FORM';
   menu  = MENU;
-  aktMenu = MENU.USERS;
 
-  public nastavMenu(m: MENU){
-    this.aktMenu = m;
+  constructor(private router: Router) {
   }
 
-  user = {id: 0, name: 'Anna Dobra', contact: 'a.dobra@example.com'}
-  users: any = [];
-
-  public addUser(): void{
-    let u = {id: this.user.id, name: this.user.name, contact: this.user.contact}
-    this.users.push(u);
-  }
-
-  book = {id: 0, name: 'The Hobbit', author: "J.R.R Tolkien", avial: 5}
-  books: any = [];
-
-  public addBook(): void{
-    let b = {id: this.book.id, name: this.book.name, author: this.book.author, avialable: this.book.avial}
-    this.books.push(b);
-  }
-
-  borrowing = {id: 0, book: 'The Hobbit', user: "Anna Dobra"}
-  borrowings: any = [];
-
-  public addBorrowing(): void{
-    let borrow = {id: this.borrowing.id, book: this.borrowing.book, user: this.borrowing.user}
-    this.borrowings.push(borrow);
+  otvorMenu(m: MENU){
+    if(m == MENU.USERS){
+      this.router.navigate(['/users']);
+    }
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
+import {CustomerFormularComponent} from "./customer-formular/customer-formular.component";
+import {CustomerStrankaComponent} from "./customer-stranka/customer-stranka.component";
+import {CustomerZoznamComponent} from "./customer-zoznam/customer-zoznam.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerFormularComponent,
+    CustomerStrankaComponent,
+    CustomerZoznamComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
