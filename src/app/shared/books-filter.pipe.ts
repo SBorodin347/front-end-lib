189267eeebb40ea6bd25cd2a981a257a00d5852d
Dominika Kamenska <dominika.kamenska@r-das.sk>
2023-01-09 19:01:14 +0100

    docker

--- src/app/shared/books-filter.pipe.ts
+++ src/app/shared/books-filter.pipe.ts
@@ -10,7 +10,8 @@ export class BooksFilterPipe implements PipeTransform{
       return books;
     }
     return books.filter(book=>{
-      return book.authorName.toLocaleLowerCase().indexOf(search.toLocaleLowerCase()) !== -1 || book.title.toLocaleLowerCase().indexOf(search.toLocaleLowerCase()) !== -1;
+      let authorName = book.authorFirstName + book.authorLastName;
+      return authorName.toLocaleLowerCase().indexOf(search.toLocaleLowerCase()) !== -1 || book.title.toLocaleLowerCase().indexOf(search.toLocaleLowerCase()) !== -1;
     })
   }
 }
